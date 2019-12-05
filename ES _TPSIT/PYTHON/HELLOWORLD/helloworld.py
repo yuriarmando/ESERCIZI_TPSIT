32e9d443255b802d2d8934498e68f284ffc32f7d
Yuri Armando <48619228+yuriarmando@users.noreply.github.com>
2019-12-05 09:06:59 +0100

    First pythonb

---
+++ ES _TPSIT/PYTHON/HELLOWORLD/helloworld.py
@@ -0,0 +1,10 @@
+print("Hello world")
+x=10
+print(f"x vale : {x}") 
+for i in range(0,10):       #PROGRAMMA DI ESEMPIO
+    if(i%2==0):
+        print(f"{i} è pari")
+    else:
+        print(f"{i} è dispari")
+
+    
